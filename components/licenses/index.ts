9bcd57676d33aeaab68f60ad24c051323e074f06
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2021-07-14 15:23:01 -0400

    Adding Apache 2.0 and LGPL v3

--- components/licenses/index.ts
+++ components/licenses/index.ts
@@ -1,9 +1,13 @@
+import { license as apache20 } from "./Apache-2.0";
 import { license as GPLv3 } from "./GPLv3";
+import { license as LGPLv3 } from "./LGPLv3"
 import { license as MIT } from "./MIT";
 
 export namespace Licenses {
     export const licenses: array = [
+        apache20,
         GPLv3,
+        LGPLv3,
         MIT,
     ];
 };
