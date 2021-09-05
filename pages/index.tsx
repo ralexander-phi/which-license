c6cce519b1d6374ebdcf6931d60ee87c0a7a7552
Robert <3751418+ralexander-phi@users.noreply.github.com>
2021-09-05 16:37:02 -0400

    Helmet for head content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import 'bulma/css/bulma.css'
+import {Helmet} from "react-helmet";
 
 import DetectLicense from '../components/detect';
 
@@ -12,24 +13,25 @@ export default function Home({
   }[]
 }) {
   return (
-    <html>
-      <head>Which License?</head>
-      <body>
-        <nav className="navbar is-dark pb-2" role="navigation" aria-label="main navigation">
-          <div className="navbar-brand">
-            <h1 className="title p-4 has-text-light">Which License?</h1>
-          </div>
-        </nav>
+    <>
+    <Helmet>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Which License?</title>
+    </Helmet>
+    <nav className="navbar is-dark pb-2" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+    <h1 className="title p-4 has-text-light">Which License?</h1>
+    </div>
+    </nav>
 
-        <section className="section">
-          <DetectLicense />
-        </section>
+    <section className="section">
+    <DetectLicense />
+    </section>
 
-        <footer className="footer">
-          <p>Information is provided without warranty.</p>
-          <p>This is not legal advice</p>
-        </footer>
-      </body>
-    </html>
+    <footer className="footer">
+    <p>Information is provided without warranty.</p>
+    <p>This is not legal advice</p>
+    </footer>
+    </>
   )
 }
