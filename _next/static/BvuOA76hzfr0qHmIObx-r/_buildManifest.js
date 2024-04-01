218bbf62390e8453ebc6d1250b6577181233f2ad
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2024-04-01 12:58:21 +0400

    Deploy to gh-pages

--- _next/static/BvuOA76hzfr0qHmIObx-r/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/9ffeea93daed4b2f.css","static/chunks/pages/index-f007924bba86ea74.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
