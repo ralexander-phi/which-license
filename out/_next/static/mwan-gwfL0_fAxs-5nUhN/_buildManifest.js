7da8225881c5df0d4789857e4f9d419de910a572
Robert Alexander <robert@robalexdev.com>
2024-07-29 21:11:56 -0400

    Deploy to gh-pages

---
+++ out/_next/static/mwan-gwfL0_fAxs-5nUhN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/2770cbc5d418c159.css","static/chunks/pages/index-f30be0b1f6499eec.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
